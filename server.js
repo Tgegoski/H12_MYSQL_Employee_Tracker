ff8f23b2184a874349f9dd3e874163e9b041614f
Tammy Gegoski <tgegoski@gmail.com>
2021-04-17 12:29:29 -0400

    set up view functions

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const util = require('util');
 const cTable = require('console.table');
 
 const connection = mysql.createConnection({
   host: 'localhost',
 
-  // Your port; if not 3306
   port: 3306,
 
-  // Your username
   user: 'root',
 
-  // Be sure to update with your own MySQL password!
   password: 'Jasper#02',
   database: 'employee_tracker',
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  runSearch();
 });
